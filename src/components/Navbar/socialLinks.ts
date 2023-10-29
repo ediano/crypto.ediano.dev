8ee9d8a25799df7f3f4ef418de1458d798ebad14
Ediano Silva Santos <silva.ediano.santos@gmail.com>
2023-10-28 21:25:53 -0400

    Update socialLinks.ts

--- src/components/Navbar/socialLinks.ts
+++ src/components/Navbar/socialLinks.ts
@@ -1,3 +1,3 @@
 import { GithubIco } from './Icons';
 
-export const socialLinks = [{ name: 'GitHub', Icon: GithubIco, url: 'https://github.com/ediano/crypto.ediano.xyz' }];
+export const socialLinks = [{ name: 'GitHub', Icon: GithubIco, url: 'https://github.com/ediano/crypto.ediano.dev' }];
